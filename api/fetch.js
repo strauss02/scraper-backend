cad39c19749a5b7ba73192440d08264912a2ae40
Ido Strauss <strauss02@hotmail.co.il>
2022-02-07 00:26:57 +0200

    modulize fetch function

--- api/fetch.js
+++ api/fetch.js
@@ -3,27 +3,37 @@ import http from 'http'
 import https from 'https'
 import SocksProxyAgent from 'socks-proxy-agent'
 import config from './config.js'
+import util from 'util'
+import { resolve } from 'path'
+import bl from 'bl'
 
-// Use the SOCKS_PROXY env var if using a custom bind address or port for your TOR proxy:
+export default async function fetchHTML() {
+  const proxy = process.env.SOCKS_PROXY || 'socks5h://127.0.0.1:9050'
+  console.log('Using proxy server %j', proxy)
+  const endpoint =
+    config.STRONGHOLD_URL ||
+    'https://duckduckgogg42xjoc72x3sjasowoarfbgcmvfimaftt6twagswzczad.onion'
+  console.log('Attempting to GET %j', endpoint)
+  // Prepare options for the http/s module by parsing the endpoint URL:
+  let options = parse(endpoint)
+  const agent = new SocksProxyAgent(proxy)
+  // Here we pass the socks proxy agent to the http/s module:
+  options.agent = agent
+  // Depending on the endpoint's protocol, we use http or https module:
+  const httpOrHttps = options.protocol === 'https:' ? https : http
+  // Make an HTTP GET request:
 
-const proxy = process.env.SOCKS_PROXY || 'socks5h://127.0.0.1:9050'
-console.log('Using proxy server %j', proxy)
-// The default HTTP endpoint here is DuckDuckGo's v3 onion address:
-const endpoint =
-  config.STRONGHOLD_URL ||
-  'https://duckduckgogg42xjoc72x3sjasowoarfbgcmvfimaftt6twagswzczad.onion'
-console.log('Attempting to GET %j', endpoint)
-// Prepare options for the http/s module by parsing the endpoint URL:
-let options = parse(endpoint)
-const agent = new SocksProxyAgent(proxy)
-// Here we pass the socks proxy agent to the http/s module:
-options.agent = agent
-// Depending on the endpoint's protocol, we use http or https module:
-const httpOrHttps = options.protocol === 'https:' ? https : http
-// Make an HTTP GET request:
-httpOrHttps.get(options, (res) => {
-  // Print headers on response:
-  console.log('Response received', res.headers)
-  // Pipe response body to output stream:
-  res.pipe(process.stdout)
-})
+  return new Promise((resolve, reject) => {
+    httpOrHttps.get(options, (response) => {
+      // response.setEncoding('utf8')
+      response.pipe(
+        bl((err, data) => {
+          if (err) {
+            reject(err)
+          }
+          resolve(data.toString())
+        })
+      )
+    })
+  })
+}
--- api/test.js
+++ api/test.js
@@ -1,21 +1,6 @@
-import fetch from 'node-fetch'
-// const HttpsProxyAgent = require('https-proxy-agent')
-import HttpsProxyAgent from 'https-proxy-agent'
-const test = async () => {
-  const proxyAgent = new HttpsProxyAgent('http://141.226.8.53:8080')
-  const response = await fetch('https://httpbin.org/ip?json', {
-    agent: proxyAgent,
-  })
-  const body = await response.text()
-  console.log(body)
-}
+import fetchHTML from './fetch.js'
 
-test()
+fetchHTML().then((res) => console.log(res))
 
-// const URL =
-//   'socks5h://127.0.0.1:9050 http://strongerw2ise74v3duebgsvug4mehyhlpa7f6kfwnas7zofs3kov7yd.onion/all'
-
-// const response = await fetch(URL)
-// const body = await response.text()
-
-// console.log(body)
+// console.log('im here')
+// console.log(response)
