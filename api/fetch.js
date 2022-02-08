f0946d24315d1031d0009965085307d8e3182e05
Ido Strauss <strauss02@hotmail.co.il>
2022-02-08 21:37:28 +0200

    implement getAllEntriesURLs func

--- api/fetch.js
+++ api/fetch.js
@@ -7,7 +7,7 @@ import util from 'util'
 import { resolve } from 'path'
 import bl from 'bl'
 
-export default async function fetchHTML(URL) {
+export async function fetchHTML(URL) {
   const proxy = process.env.SOCKS_PROXY || 'socks5h://127.0.0.1:9050'
   console.log('Using proxy server %j', proxy)
   const endpoint = URL
@@ -24,6 +24,10 @@ export default async function fetchHTML(URL) {
   return new Promise((resolve, reject) => {
     httpOrHttps.get(options, (response) => {
       // response.setEncoding('utf8')
+      console.log(response.statusCode)
+      if (response.statusCode == 418) {
+        reject('page does not exist')
+      }
       response.pipe(
         bl((err, data) => {
           if (err) {
@@ -32,6 +36,23 @@ export default async function fetchHTML(URL) {
           resolve(data.toString())
         })
       )
+    })
+  })
+}
+
+export function checkPageExistence(endpoint) {
+  const proxy = process.env.SOCKS_PROXY || 'socks5h://127.0.0.1:9050'
+  console.log('Using proxy server %j', proxy)
+  console.log('Attempting to GET %j', endpoint)
+  // Prepare options for the http/s module by parsing the endpoint URL:
+  let options = parse(endpoint)
+  const agent = new SocksProxyAgent(proxy)
+  options.agent = agent
+
+  return new Promise((resolve, reject) => {
+    http.get(options, (response) => {
+      console.log(response.statusCode)
+      resolve(response.statusCode == 418 ? false : true)
     })
   })
 }
--- api/test.js
+++ api/test.js
@@ -1,14 +1,39 @@
-import fetchHTML from './fetch.js'
+import { fetchHTML, checkPageExistence } from './fetch.js'
 import { parseInfoFromEntry, parseButtonLinks } from './utils.js'
 import config from './config.js'
 
-const response = await fetchHTML(config.STRONGHOLD_URL)
+async function getAllEntriesURLs() {
+  let pageNum = 0
+  let urlTemplate = `${config.STRONGHOLD_URL}?page=${pageNum}`
+  let entriesURLs = []
+  let pageExistence = await checkPageExistence(urlTemplate)
+  while (pageExistence) {
+    await fetchHTML(urlTemplate)
+      .then((res) => {
+        const entryLinks = parseButtonLinks(res)
+        entriesURLs = entriesURLs.concat(entryLinks)
+        pageNum += 1
+        urlTemplate = `${config.STRONGHOLD_URL}?page=${pageNum}`
+      })
+      .catch((err) => {
+        pageExistence = false
+      })
+    console.log(pageNum, pageExistence, entriesURLs)
+  }
+  // Remove all the undefined values
+  entriesURLs = entriesURLs.filter((item) => item)
+  return entriesURLs
+}
 
-const testEntryURL = parseButtonLinks(response)[0]
-console.log(testEntryURL)
+// const response = await fetchHTML(config.STRONGHOLD_URL)
 
-const testEntry = await fetchHTML(testEntryURL)
+// const testEntryURL = parseButtonLinks(response)[0]
+// console.log(testEntryURL)
+
+// const testEntry = await fetchHTML(testEntryURL)
 
 // console.log(testEntry)
 
-console.log(parseInfoFromEntry(testEntry))
+// console.log(parseInfoFromEntry(testEntry))
+
+console.log('get urls:', await getAllEntriesURLs())
