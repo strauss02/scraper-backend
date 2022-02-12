59908112a3a81c64a05de4b7e64cf3ff2aee7a0e
Ido Strauss <strauss02@hotmail.co.il>
2022-02-12 13:55:48 +0200

    add basic error handling for job

--- api/server/index.js
+++ api/server/index.js
@@ -15,7 +15,7 @@ db.Entries.find({}, {date:1}).sort({_id:1}).forEach(function(doc){
     db.Entries.deleteOne({_id:{$gt:doc._id}, date:doc.date});
 })
 
-/*************** */
+****************/
 
 //: TODO: Add error handling.
 
@@ -31,36 +31,43 @@ await mongoose
 export let newestEntryDateString = '10 Feb 2022, 21:28:42 UTC '
 
 cron.schedule('*/1 * * * *', async () => {
-  console.log('running a task every two minutes')
-  //
-  const latestEntry = await entry.findOne().sort({ date: -1 }).limit(1)
-  console.log('latest entry', latestEntry)
-  // convert to string
-  const convertedDate = latestEntry['date'].toString()
-  console.log('converted date', convertedDate)
-  console.log(
-    'this is the latest entry date: ',
-    convertedDate,
-    'Getting entries later than this date.'
-  )
-  newestEntryDateString = convertedDate
-  //
-  const data = await getAllNewEntriesParsedInfo()
-  console.log('All pastes scraped and parsed. ')
-  console.log('this is the raw data, right after scraping and parsing:', data)
-  //
-  // newestEntryDateString = await getNewestEntryDate(data)
-  // console.log('Got latest entry date: ', newestEntryDateString)
-  //
-  const analyzedData = await addAnalysisToEntries(data, client)
-  console.log('Data has been assigned with analysis.')
-  await storeEntries(analyzedData)
-  console.log('Data successfuly stored. Scraping job completed.')
-  console.log(
-    'latest date is currently:',
-    newestEntryDateString,
-    typeof newestEntryDateString
-  )
+  try {
+    console.log('running a task every two minutes')
+    //
+    const latestEntry = await entry.findOne().sort({ date: -1 }).limit(1)
+    console.log('latest entry', latestEntry)
+    // convert to string
+    const convertedDate = latestEntry['date'].toString()
+    console.log('converted date', convertedDate)
+    console.log(
+      'this is the latest entry date: ',
+      convertedDate,
+      'Getting entries later than this date.'
+    )
+    newestEntryDateString = convertedDate
+    //
+    const data = await getAllNewEntriesParsedInfo()
+    console.log('All pastes scraped and parsed. ')
+    console.log('this is the raw data, right after scraping and parsing:', data)
+    //
+    // newestEntryDateString = await getNewestEntryDate(data)
+    // console.log('Got latest entry date: ', newestEntryDateString)
+    //
+    const analyzedData = await addAnalysisToEntries(data, client)
+    console.log('Data has been assigned with analysis.')
+    await storeEntries(analyzedData)
+    console.log('Data successfuly stored. Scraping job completed.')
+    console.log(
+      'latest date is currently:',
+      newestEntryDateString,
+      typeof newestEntryDateString
+    )
+  } catch (error) {
+    console.log(
+      'Something went wrong while executing scheduled scraping task. Error:',
+      error
+    )
+  }
 })
 
 async function storeEntries(data) {
